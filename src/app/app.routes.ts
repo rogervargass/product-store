b1ba59a4a216094b0c15d7ba17a3c0ece46a9b31
Roger Vargas <rogerv@abcbrasil.com.br>
2024-08-19 16:39:55 -0300

    refactor: carrega CreateComponent via lazy loading

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,16 @@
 import { Routes } from '@angular/router';
-import { CreateComponent } from './features/create/create.component';
 import { ListComponent } from './features/list/list.component';
 
 export const routes: Routes = [
   {
     path: '',
-    component: ListComponent
+    component: ListComponent,
   },
   {
     path: 'create-product',
-    component: CreateComponent
+    loadComponent: () =>
+      import('./features/create/create.component').then(
+        (m) => m.CreateComponent
+      ),
   },
 ];
